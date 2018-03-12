377c76532dc0a55047c693e7998b8b4bcf55f10c
t5i0m7 <p840507@gmail.com>
2018-03-13 00:08:53 +0800

    DBJ hash table

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -3,11 +3,15 @@
 
 #define MAX_LAST_NAME_SIZE 16
 
+#define HASH_TABLE_SIZE 1024 // 2^10 size
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
+#define OPT 1
+
+
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
+    /*
     char firstName[16];
     char email[16];
     char phone[10];
@@ -15,11 +19,15 @@ typedef struct __PHONE_BOOK_ENTRY {
     char addr1[16];
     char addr2[16];
     char city[16];
-    char state[2];
+    char state[2];*/
     char zip[5];
+
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
+entry hash_table[HASH_TABLE_SIZE];
+unsigned int DJBHash(char lastName[]);
+unsigned int DJBHash(char lastName[]);
 entry *findName(char lastName[], entry *pHead);
 entry *append(char lastName[], entry *e);
 
