78cc0fe4530ce387097b6bf94b51bad0e17c24c3
t5i0m7 <p840507@gmail.com>
2018-03-13 01:22:59 +0800

    Reduce entry size

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -3,14 +3,27 @@
 #include "phonebook_opt.h"
 
 /* TODO: FILL YOUR OWN IMPLEMENTATION HERE! */
+// this is DJB implement on findname and append
+
+
+/* original version */
 entry *findName(char lastName[], entry *pHead)
 {
-    /* TODO: implement */
+    while (pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    /* TODO: implement */
-    return NULL;
+    /* allocate memory for the new entry and put lastName */
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+
+    return e;
 }
